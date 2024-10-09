e50e75978ddead53533fb7011aa0775350f13c97
Karlos Warney <karloswarney@gmail.com>
2024-10-09 16:44:58 -0300

    add the class firebaseCrud to the root vue element.

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -2,10 +2,16 @@
 import vuetify from './vuetify'
 import pinia from '@/stores'
 import router from '@/router'
+import FirebaseCRUD from "@/plugins/FirebaseCRUD"
+
+function install(app){
+  app.provide('firebase', FirebaseCRUD)
+}
 
 export function registerPlugins (app) {
   app
     .use(vuetify)
     .use(router)
     .use(pinia)
+    .use(install)
 }
