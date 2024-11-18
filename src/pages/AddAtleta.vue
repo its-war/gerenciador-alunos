e583ff225bc9907c781e8470c7a309eeee8cc01a
ValberMaia <maialencar2017@gmail.com>
2024-11-18 12:36:37 -0300

    inicio do Layout da pagina de registro de atleta (problema de rota)

---
+++ src/pages/AddAtleta.vue
@@ -0,0 +1,20 @@
+<template>
+<AppBar/>
+<v-main>
+    <router-view />
+</v-main>
+</template>
+
+<script>
+import AppBar from "@/components/add-atleta-page/AppBar.vue";
+
+export default {
+    name: "AddAtleta",
+    components: {AppBar}
+}
+</script>
+
+<style scoped>
+
+</style>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 // Routes Components
 import HomePage from "@/pages/HomePage.vue";
 import InicioComponent from "@/components/home-page/InicioComponent.vue";
+import AddAtleta from "@/pages/AddAtleta.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
+      path: "/", // Página inicial padrão
       component: HomePage,
       children: [
         {
           path: "",
           name: "Home",
-          component: InicioComponent
-        }
-      ]
-    }
-  ]
-})
+          component: InicioComponent,
+        },
+      ],
+    },
+    {
+      path: "/add-atleta", // Caminho para AddAtleta
+      name: "AddAtleta",
+      component: AddAtleta,
+    },
+  ],
+});
 
-export default router
+export default router;
