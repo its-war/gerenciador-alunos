ee375ec926df9293e4f9e30761927eecfb73b9c5
ValberMaia <maialencar2017@gmail.com>
2024-11-18 11:39:18 -0300

    layout da Home page 90% feito

--- src/components/home-page/AppBar.vue
+++ src/components/home-page/AppBar.vue
@@ -8,7 +8,7 @@
             <v-icon>mdi-arrow-left</v-icon>
           </v-btn>
         </v-col>
-        <v-col class="d-flex justify-center">
+        <v-col class="d-flex justify-center" style="flex: 60%;">
           <h3>Pagina principal</h3>
         </v-col>
         <v-col class="d-flex justify-end">
@@ -17,7 +17,6 @@
       </v-row>
 
       <!-- Botões principais -->
-      <!-- Botões principais com altura maior e largura reduzida -->
       <v-row justify="center">
         <v-btn
           class="mb-6 button-custom"
@@ -31,11 +30,14 @@
           outlined
         >Gerenciador de Turmas</v-btn>
 
-        <v-btn
+        <v-btn @click="goToAddAtleta"
           class="mb-6 button-custom"
           style="width: 70%; height: 70px; font-size: 18px;"
           outlined
-        >Cadastro de Alunos</v-btn>
+        >
+          Cadastro de Alunos
+        </v-btn>
+
 
         <v-btn
           class="mb-6 button-custom"
@@ -54,18 +56,18 @@
           <v-icon>mdi-home</v-icon>
         </v-btn>
 
-        <v-btn>
-          <span>Gerenciador</span>
+        <v-btn @click="toggleManagePanel">
+          <span>Gerenciar</span>
           <v-icon>mdi-format-list-bulleted</v-icon>
         </v-btn>
 
-        <v-btn>
+        <v-btn @click="toggleAddPanel">
           <span>Adicionar</span>
           <v-icon>mdi-plus</v-icon>
         </v-btn>
 
         <v-btn>
-          <span>Configurações</span>
+          <span>Opções</span>
           <v-icon>mdi-cog</v-icon>
         </v-btn>
 
@@ -75,18 +77,71 @@
         </v-btn>
 
       </v-bottom-navigation>
+
+
+        <!-- Área de clique fora -->
+      <div v-if="showManagePanel || showAddPanel" class="overlay" @click="closePanels"></div>
+
+      <!-- Painel de Gerenciamento -->
+      <div v-if="showManagePanel" class="manage-panel">
+        <v-btn @click="manageStudents">Gerenciar Alunos</v-btn>
+        <v-btn @click="manageClasses">Gerenciar Turmas</v-btn>
+      </div>
+
+      <!-- Painel de Adição -->
+      <div v-if="showAddPanel" class="add-panel">
+        <v-btn @click="addStudent">Adicionar Aluno</v-btn>
+        <v-btn @click="addClass">Adicionar Turma</v-btn>
+      </div>
+
     </v-container>
   </v-app>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      showManagePanel: false,
+      showAddPanel: false
+    };
+  },
   methods: {
     goBack() {
-      // Lógica para voltar à página anterior
+      // voltar à página anterior
     },
     goHome() {
-      // Lógica para ir à página inicial
+      // ir à página inicial
+    },
+    toggleManagePanel() {
+      this.showManagePanel = !this.showManagePanel;
+      // Garante que apenas um painel estará aberto por vez
+      if (this.showManagePanel) this.showAddPanel = false;
+    },
+    toggleAddPanel() {
+      this.showAddPanel = !this.showAddPanel;
+      // Garante que apenas um painel estará aberto por vez
+      if (this.showAddPanel) this.showManagePanel = false;
+    },
+    closePanels() {
+      // Fecha qualquer painel que estiver aberto
+      this.showManagePanel = false;
+      this.showAddPanel = false;
+    },
+    manageStudents() {
+      console.log("Gerenciar Alunos clicado");
+    },
+    manageClasses() {
+      console.log("Gerenciar Turmas clicado");
+    },
+    addStudent() {
+      console.log("Adicionar Aluno clicado");
+    },
+    addClass() {
+      console.log("Adicionar Turma clicado");
+    },
+    goToAddAtleta() {
+      this.$router.push({ name: "AddAtleta" });
     },
   },
 };
@@ -112,11 +167,39 @@ h3 {
   position: fixed;
   bottom: 0;
   width: 100%;
+  box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.3);
 
   .v-btn{
     min-width: 20% !important;
     max-width: 20% !important;
+    padding: 0%;
   }
 }
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); 
+  z-index: 10; 
+}
 
+.manage-panel, .add-panel {
+  position: fixed;
+  bottom: 7.5%;
+  left: 0;
+  width: 100%;
+  height: 10vh;
+  background-color: rgba(33,33,33,255);
+  box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  z-index: 20;
+
+  .v-btn{
+    background-color: rgba(69,69,69,255);
+  }
+}
 </style>
